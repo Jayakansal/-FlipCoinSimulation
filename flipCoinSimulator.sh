e5e7e0d6f7145aad5563b8717f9b9ef12e7d4adb
Jayakansal <21jayajindal@gmail.com>
2020-11-21 23:18:27 +0530

    this is for multiple times of flipping

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,18 @@
+heads=0;
+tails=0;
+
+while [ "$heads" -lt 5 ]
+do
+
  a=$(( RANDOM%2 ));
         if [ $a == 1 ];
         then
-                echo "It is a head"
+               heads=$(($heads+1))
+
         else
-                echo "It is a tail"
+                tails=$(($tails+1))
+
         fi
+done
+echo "final heads value" $heads
+echo "final tails value" $tails

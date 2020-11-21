3208d6bf8b94a9aa283392119bd5b6a41ed3ef57
Jayakansal <21jayajindal@gmail.com>
2020-11-21 23:40:27 +0530

    match won by how much

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,18 +1,23 @@
 heads=0;
 tails=0;
-
-while [ "$heads" -lt 5 ]
+while [ "$heads" -lt 21 ] && [ "$tails" -lt 21 ]
 do
-
- a=$(( RANDOM%2 ));
+        a=$(( RANDOM%2 ));
         if [ $a == 1 ];
         then
-               heads=$(($heads+1))
-
+                heads=$(($heads+1))
         else
                 tails=$(($tails+1))
-
         fi
 done
+
+if [ "$heads" -eq 21 ] && [ "$tails" -eq 21 ]
+then
+        if [ "$heads" -eq 21 ]
+
+then    echo "heads won by " $(($heads-$tails))
+else
+        echo "tails won by " $(($tails-$heads))
+fi
 echo "final heads value" $heads
-echo "final tails value" $tails
+echo "final tails value" $tail
